436cc6e32c1adbd588b072a02925883e742bfe9b
Graham Turner <tgturner@outlook.com>
2016-03-14 02:57:33 -0400

    Added keyboard navigation YT fixes for navigating light box

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ $(".photoView").append("<a class='linkBackButton' href='#'><img class='backButto
 $(".photoView").append("<a class='linkNextButton' href='#'><img class='nextButton' src='img/arrowright.png'></a>");
 $(".photoView").append("<a class='linkCloseButton' href='#'><img class='closeButton' src='img/close-icon.png'></a>");
 
+//Searching
+
 $("#liveSearch").keyup(function() {
     var search = $(this).val();
     console.log(search);
@@ -21,21 +23,35 @@ $("#liveSearch").keyup(function() {
     });
 });
 
+//Creating lightbox on click
+
 $(".gallery a").on("click", function(event) {
     event.preventDefault();
-    var $imgSource = $(this).attr("href");
-    var $imgText = $(this).children().text();
-    thisImage = $(this);
-    thisImageText = $(this).children().text();
-    $(".shade").show();
-    $(".shadeImg").attr("src", $imgSource);  
-    $(".shade p").text($imgText);
+    console.log($(this).hasClass("ytvid"));
+    if($(this).hasClass("ytvid") == true) {
+        $(".shadeVid").show();
+        $(".shadeImg").hide();
+        var $imgSource = $(this).attr("href");
+        var $imgText = $(this).children().text();
+        thisImage = $(this);
+        thisImageText = $(this).children().text();
+        $(".shade").show();
+        $(".shadeVid").attr("src", $imgSource);  
+        $(".shade p").text($imgText);
+    } else {
+        $(".shadeImg").show();
+        $(".shadeVid").hide();
+        var $imgSource = $(this).attr("href");
+        var $imgText = $(this).children().text();
+        thisImage = $(this);
+        thisImageText = $(this).children().text();
+        $(".shade").show();
+        $(".shadeImg").attr("src", $imgSource);  
+        $(".shade p").text($imgText);
+    }
 });
 
-$(".ytvid").on("click", function(event) {
-    event.preventDefault();
-    console.log("hello");
-});
+//Creating targets for navigating and closing lightbox
 
 $(".linkCloseButton").on("click", function(event) {
     event.preventDefault();
@@ -44,25 +60,108 @@ $(".linkCloseButton").on("click", function(event) {
 
 $(".linkNextButton").on("click", function() {
     event.preventDefault();
-    if(thisImage.next().attr("href") !== undefined) {
+    if(thisImage.next().attr("href") !== undefined && thisImage.next().hasClass("ytvid") == true) {
+        $(".shadeImg").hide();
+        $(".shadeVid").show();
         var nextSource = $(thisImage).next().attr("href");
         var nextText = $(thisImage).next().children().text();
+        console.log(nextText);
         console.log(nextSource);
-        $(".shadeImg").attr("src", nextSource);
+        $(".shadeElement").attr("src", nextSource);
         $(".shade p").text(nextText);
         thisImage = thisImage.next();
-    } else {console.log("Hi!")}
+    } else if(thisImage.next().attr("href") !== undefined) {
+        console.log("YOYOYO")
+        $(".shadeVid").hide();
+        $(".shadeImg").show();
+        var nextSource = $(thisImage).next().attr("href");
+        var nextText = $(thisImage).next().children().text();
+        console.log(nextText);
+        console.log(nextSource);
+        $(".shadeElement").attr("src", nextSource);
+        $(".shade p").text(nextText);
+        thisImage = thisImage.next();
+    }
 });
 
 $(".linkBackButton").on("click", function(event) {
     event.preventDefault();
-    if(thisImage.prev().attr("href") !== undefined) {
+    if(thisImage.prev().attr("href") !== undefined && thisImage.prev().hasClass("ytvid") == true) {
+        $(".shadeImg").hide();
+        $(".shadeVid").show();
         var prevSource = $(thisImage).prev().attr("href");
         var prevText = $(thisImage).prev().children().text();
         console.log(prevText);
         console.log(prevSource);
-        $(".shadeImg").attr("src", prevSource);
+        $(".shadeElement").attr("src", prevSource);
         $(".shade p").text(prevText);
         thisImage = thisImage.prev();
-    } else {console.log("Sorry!")}
+    } else if(thisImage.prev().attr("href") !== undefined)  {
+        $(".shadeVid").hide();
+        $(".shadeImg").show();
+        var prevSource = $(thisImage).prev().attr("href");
+        var prevText = $(thisImage).prev().children().text();
+        console.log(prevText);
+        console.log(prevSource);
+        $(".shadeElement").attr("src", prevSource);
+        $(".shade p").text(prevText);
+        thisImage = thisImage.prev();
+    }
 });
+
+//Binding buttons for navigating lightbox
+
+Mousetrap.bind('left', function() {
+    if($('.shade').is(':visible')) {
+        event.preventDefault();
+        if(thisImage.prev().attr("href") !== undefined && thisImage.prev().hasClass("ytvid") == true) {
+            $(".shadeImg").hide();
+            $(".shadeVid").show();
+            var prevSource = $(thisImage).prev().attr("href");
+            var prevText = $(thisImage).prev().children().text();
+            console.log(prevText);
+            console.log(prevSource);
+            $(".shadeElement").attr("src", prevSource);
+            $(".shade p").text(prevText);
+            thisImage = thisImage.prev();
+        } else if(thisImage.prev().attr("href") !== undefined)  {
+            $(".shadeVid").hide();
+            $(".shadeImg").show();
+            var prevSource = $(thisImage).prev().attr("href");
+            var prevText = $(thisImage).prev().children().text();
+            console.log(prevText);
+            console.log(prevSource);
+            $(".shadeElement").attr("src", prevSource);
+            $(".shade p").text(prevText);
+            thisImage = thisImage.prev();
+        } else {console.log("help!")}
+    }
+});
+
+Mousetrap.bind('right', function() {
+    if($('.shade').is(':visible')) {
+        event.preventDefault();
+        if(thisImage.next().attr("href") !== undefined && thisImage.next().hasClass("ytvid") == true) {
+            $(".shadeImg").hide();
+            $(".shadeVid").show();
+            var nextSource = $(thisImage).next().attr("href");
+            var nextText = $(thisImage).next().children().text();
+            console.log(nextText);
+            console.log(nextSource);
+            $(".shadeElement").attr("src", nextSource);
+            $(".shade p").text(nextText);
+            thisImage = thisImage.next();
+        } else if(thisImage.next().attr("href") !== undefined) {
+            $(".shadeVid").hide();
+            $(".shadeImg").show();
+            var nextSource = $(thisImage).next().attr("href");
+            var nextText = $(thisImage).next().children().text();
+            console.log(nextText);
+            console.log(nextSource);
+            $(".shadeElement").attr("src", nextSource);
+            $(".shade p").text(nextText);
+            thisImage = thisImage.next();
+        }
+    }
+});
+
